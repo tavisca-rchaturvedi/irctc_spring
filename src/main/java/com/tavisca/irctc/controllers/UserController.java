03fb79e6ed9cc2998be05a5193a2c05e330ef39d
Mandal <vmandal@internal.tavisca.com>
2019-12-21 21:59:25 +0530

    searchByStationName done

--- src/main/java/com/tavisca/irctc/controllers/UserController.java
+++ src/main/java/com/tavisca/irctc/controllers/UserController.java
@@ -1,11 +1,14 @@
 package com.tavisca.irctc.controllers;
 
 
+import java.util.List;
 import com.tavisca.irctc.dao.StopRepo;
 import com.tavisca.irctc.dao.TrainRepo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -13,14 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-//    @Autowired
-//    TrainRepo trainRepo;
-//
-//    @Autowired
-//    StopRepo stopRepo;
+    @Autowired
+    TrainRepo trainRepo;
 
-//    @GetMapping("/searchTrains/{source}/{destination}")
-//    public List<Object> searchTrains(@PathVariable String source, @PathVariable String destination){
-//        return stopRepo.searchTrain(source,destination);
-//    }
+    @Autowired
+    StopRepo stopRepo;
+
+    @GetMapping("/searchTrains/{source}/{destination}")
+    public List<Object> searchTrains(@PathVariable String source, @PathVariable String destination){
+        return stopRepo.searchTrain(source,destination);
+    }
 }
