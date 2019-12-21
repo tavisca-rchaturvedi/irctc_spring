764043db26d9cd61c996d28bd0d7d452f07f3452
Mandal <vmandal@internal.tavisca.com>
2019-12-21 22:35:05 +0530

    implemented searchByTrain in service layer(refactor)

--- src/main/java/com/tavisca/irctc/services/UserService.java
+++ src/main/java/com/tavisca/irctc/services/UserService.java
@@ -1,4 +1,17 @@
 package com.tavisca.irctc.services;
 
+import java.util.List;
+import com.tavisca.irctc.dao.StopRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    StopRepo stopRepo;
+
+    public List<Object> searchTrainByStation(String source,String destination){
+        return stopRepo.searchTrain(source,destination);
+    }
 }
