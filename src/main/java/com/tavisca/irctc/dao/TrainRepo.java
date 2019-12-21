467b64f03500b2b14c38a841c186ff2652d5c456
Mandal <vmandal@internal.tavisca.com>
2019-12-21 23:02:38 +0530

    searchByTrainId implemented

--- src/main/java/com/tavisca/irctc/dao/TrainRepo.java
+++ src/main/java/com/tavisca/irctc/dao/TrainRepo.java
@@ -11,5 +11,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TrainRepo extends JpaRepository<Train,Integer> {
 
-
 }
--- src/main/java/com/tavisca/irctc/services/UserService.java
+++ src/main/java/com/tavisca/irctc/services/UserService.java
@@ -1,7 +1,11 @@
 package com.tavisca.irctc.services;
 
 import java.util.List;
+import java.util.Optional;
+
 import com.tavisca.irctc.dao.StopRepo;
+import com.tavisca.irctc.dao.TrainRepo;
+import com.tavisca.irctc.models.Train;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +13,15 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     @Autowired
+    TrainRepo trainRepo;
+    @Autowired
     StopRepo stopRepo;
 
     public List<Object> searchTrainByStation(String source,String destination){
         return stopRepo.searchTrain(source,destination);
+    }
+
+    public Optional<Train> searchTrainByTrainId(int id) {
+        return trainRepo.findById(id);
     }
 }
